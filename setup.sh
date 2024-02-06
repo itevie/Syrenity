ed7e67d80bf571be9b04a79b9aea6fe141115fb9
Isabella <111117650+itevie@users.noreply.github.com>
2024-02-06 15:31:08 +0000

    .l,k

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-nvm install 16 && npm i -g ts-node && npm install && psql -c "CREATE DATABASE discord;" && psql -U postgres -f "sql/setup.sql";
+nvm install 16 && npm i -g ts-node && npm install && psql -c "CREATE DATABASE discord;" && psql -U postgres -d discord -f "sql/setup.sql";

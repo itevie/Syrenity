58a4fee67185845b012b90bffd1de6354d57c051
Isabella <izzyplays07@gmail.com>
2025-06-17 14:29:52 +0100

    add github webhook test

--- src/routes/api/v2/testGithub.ts
+++ src/routes/api/v2/testGithub.ts
@@ -8,10 +8,9 @@ const handler: RouteDetails = {
 
   handler: async (req, res) => {
     console.log(req.body);
-    let content = `New push on **${req.body.repository.full_name}**`;
-    let channel = await SyChannel.fetch(203);
-    let message = await SyMessage.create({
-      channelId: channel.data.id,
+    let content = `New push on **${req.body.repository.full_name}**: ${req.body.head_commit.message} (${req.body.head_commit.url}) by **${req.body.head_commit.author.username}**`;
+    await SyMessage.create({
+      channelId: 203,
       content,
       authorId: -1,
     });
